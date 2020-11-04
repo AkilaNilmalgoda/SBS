77bb7995d9333cb42223a9cac61a85feea9a4e83
unknown <akilanilmalgoda2@gmail.com>
2020-11-04 23:42:28 +0300

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is the home component</h1>
+            <Navbar/>  
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import{} from '@material-ui/core'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>This is the Navbar
+            </h2>
+
+        </div>
+    )
+}
+
+export default Navbar
