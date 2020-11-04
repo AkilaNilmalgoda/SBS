053b71e528c5e531b480e225cc9b7966a43e1434
AkilaNilmalgoda <akilanilmalgoda2@hmail.com>
2020-11-05 00:33:24 +0300

    Setup 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import{} from '@material-ui/core'
 const Navbar = () => {
     return (
         <div>
-            <h2>This is the Navbar
+            <h2>This is the Navbar of SbS
             </h2>
 
         </div>
