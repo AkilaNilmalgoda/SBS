bced6d8839f08ffd0f0e0b3cf36d277d6523e816
AkilaNilmalgoda <akilanilmalgoda2@hmail.com>
2020-11-05 14:39:32 +0300

    bootstrap tet 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <h2>This is the Navbar of SbS
 
             </h2>
-            <button className="btn btn-danger">This is the button</button> 
+            <button className="btn btn-danger">This is the btn</button> 
 
         </div>
     )
